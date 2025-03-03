fb0d9d4f68a51d9ac02d00137bfc2893c593ae9e
src19 <sr3243@srmist.edu.in>
2025-03-03 23:41:24 +0530

    Add object mapper to convert string to json

--- src/main/java/com/expense/service/dto/ExpenseDto.java
+++ src/main/java/com/expense/service/dto/ExpenseDto.java
@@ -2,6 +2,7 @@ package com.expense.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
@@ -34,4 +35,20 @@ public class ExpenseDto {
 
     @JsonProperty(value = "created_at")
     private Timestamp createdAt;
+
+    public ExpenseDto(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+            ExpenseDto expense = mapper.readValue(json, ExpenseDto.class);
+            this.externalId = expense.externalId;
+            this.amount = expense.amount;
+            this.userId = expense.userId;
+            this.merchant = expense.merchant;
+            this.currency = expense.currency;
+            this.createdAt = expense.createdAt;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to deserialize ExpenseDto from JSON", e);
+        }
+    }
 }
